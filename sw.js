d150c944846c9fb066bbb9222288bd8034ce9a8a
guilhermehnrque <guilhermehnrque@gmail.com>
2019-06-29 02:04:45 -0300

    Ajutes gerais

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const static_cache_name = 'shell-cache-v2.6';
-const dynamic_cache_name = 'dynamic-cache-v1.4';
+const static_cache_name = 'shell-cache-v2.7';
+const dynamic_cache_name = 'dynamic-cache-v1.7';
 
 const assets =[
     '/',
